b73564041d8dec9356ede224baac559874ca4a7a
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2026-03-25 15:41:43 +0100

    added review button

--- src/pages/ServiceDetail.js
+++ src/pages/ServiceDetail.js
@@ -4,7 +4,9 @@ import { useParams } from "react-router-dom";
 function ServiceDetail() {
   const { id } = useParams();
   const [service, setService] = useState(null);
-
+  const [reviews, setReviews] = useState([]);
+  const [comment, setComment] = useState("");
+  const [rating, setRating] = useState(5);
   const token = localStorage.getItem("token");
   const userEmail = localStorage.getItem("email");
 
@@ -13,10 +15,30 @@ function ServiceDetail() {
       .then((res) => res.json())
       .then((data) => setService(data))
       .catch((err) => console.log(err));
+    fetch(`http://localhost:8080/api/reviews/${id}`)
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
   }, [id]);
 
   if (!service) return <p style={{ textAlign: "center" }}>Loading...</p>;
-
+  const submitReview = () => {
+    fetch(`http://localhost:8080/api/reviews/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        comment,
+        rating,
+      }),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        setComment("");
+        setRating(5);
+        window.location.reload();
+      });
+  };
   return (
     <div style={{ padding: "40px" }}>
       <img
@@ -76,6 +98,39 @@ function ServiceDetail() {
       >
         Continue
       </button>
+      <div style={{ marginTop: "40px" }}>
+        <h3>Leave a review</h3>
+
+        <select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <option value="5">⭐⭐⭐⭐⭐</option>
+          <option value="4">⭐⭐⭐⭐</option>
+          <option value="3">⭐⭐⭐</option>
+          <option value="2">⭐⭐</option>
+          <option value="1">⭐</option>
+        </select>
+
+        <input
+          placeholder="Write a comment..."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          style={{ display: "block", marginTop: "10px", width: "100%" }}
+        />
+
+        <button onClick={submitReview} style={{ marginTop: "10px" }}>
+          Submit
+        </button>
+      </div>
+
+      <div style={{ marginTop: "30px" }}>
+        <h3>Reviews</h3>
+
+        {reviews.map((r) => (
+          <div key={r.id} style={{ marginBottom: "15px" }}>
+            <strong>{"⭐".repeat(r.rating)}</strong>
+            <p>{r.comment}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
